689e2dd725c8572933a423e5a487594eb665201a
woobinpro <woobinpro@gmail.com>
2023-03-24 18:03:21 +0900

    changed some code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,118 +1,50 @@
-/**
- * Theme: Metrica - Responsive Bootstrap 4 Admin Dashboard
- * Author: Mannatthemes
- * Module/App: Main Js
- */
-
-
 (function ($) {
 
     'use strict';
 
-    function initSlimscroll() {
-        $('.slimscroll').slimscroll({
-            height: 'auto',
-            position: 'right',
-            size: "7px",
-            color: '#7681ad',
-            wheelStep: 5,
-            touchScrollStep: 50
-        });
-    }
-
-   
-    function initMetisMenu() {
-        //metis menu
-        $("#main_menu_side_nav").metisMenu();
-    }
-
-    function initLeftMenuCollapse() {
-        // Left menu collapse
-        $('.button-menu-mobile').on('click', function (event) {
-            event.preventDefault();
-            $("body").toggleClass("enlarge-menu");
-            initSlimscroll();
-        });
-    }
-
-    function initEnlarge() {
-        if ($(window).width() < 1025) {
-            $('body').addClass('enlarge-menu');
-        } else {
-            if ($('body').data('keep-enlarged') != true)
-                $('body').removeClass('enlarge-menu');
-        }
-    }
-
-   
-
-    function initSerach() {
-        $('.search-btn').on('click', function () {
-            var targetId = $(this).data('target');
-            var $searchBar;
-            if (targetId) {
-                $searchBar = $(targetId);
-                $searchBar.toggleClass('open');
+    function getAllData() {
+        $.ajax({
+            type: "POST",
+            url: "https://trackyourcamper.com:3001/api/getAllData",
+            data: form.serialize(), // serializes the form's elements.
+            success: function(data)
+            {
+                // sessionStorage.setItem("login_status", true);
+                // $('#login_part').hide();
+                // $('#main_part').show();
             }
         });
     }
-
-
-    function initMainIconMenu() {
-        $('.main-icon-menu .nav-link').on('click', function(e){
-            e.preventDefault();
-            $(this).addClass('active');
-            $(this).siblings().removeClass('active');
-            $('.main-menu-inner').addClass('active');
-            var targ = $(this).attr('href');
-            $(targ).addClass('active');
-            $(targ).siblings().removeClass('active');
-        });
-    }
-
-    function initTooltipPlugin(){
-        $.fn.tooltip && $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="tooltip-custom"]').tooltip({
-            template: '<div class="tooltip tooltip-custom" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-        });
-    }
-    
-
-    function initActiveMenu() {
-        // === following js will activate the menu in left side bar based on url ====
-        $(".left-sidenav a").each(function () {
-            var pageUrl = window.location.href.split(/[?#]/)[0];
-            if (this.href == pageUrl) {
-                $(this).addClass("active");
-                $(this).parent().parent().addClass("in");
-                $(this).parent().parent().addClass("mm-show");
-                $(this).parent().parent().prev().addClass("active");
-                $(this).parent().parent().parent().addClass("active");
-                $(this).parent().parent().parent().addClass("mm-active");
-                $(this).parent().parent().parent().parent().addClass("in");  
-                $(this).parent().parent().parent().parent().parent().addClass("active");  
-                $(this).parent().parent().parent().parent().parent().parent().addClass("active");              
-                var menu =  $(this).closest('.main-icon-menu-pane').attr('id');
-                $("a[href='#"+menu+"']").addClass('active');
-                
-            }
-        });
-    }
-
-    
 
     function init() {
-        initSlimscroll();
-        initMetisMenu();
-        initLeftMenuCollapse();
-        initEnlarge();
-        initSerach();
-        initMainIconMenu();
-        initTooltipPlugin();
-        initActiveMenu();
-        Waves.init();
+        let login_status = sessionStorage.getItem("login_status");
+        if(!login_status) {
+            $('#main_part').hide();
+            $('#login_part').show();
+        }else {
+            $('#main_part').show();
+            $('#login_part').hide();
+        }
+        $('.btn-detail').click(function(){
+            $('#detail_form').modal();
+        });  
+        $("#admin_login").submit(function(e) {
+            e.preventDefault(); 
+            var form = $(this);
+            $.ajax({
+                type: "POST",
+                url: "https://trackyourcamper.com:3001/api/userRegister",
+                data: form.serialize(), // serializes the form's elements.
+                success: function(data)
+                {
+                    sessionStorage.setItem("login_status", true);
+                    $('#login_part').hide();
+                    $('#main_part').show();
+                    getAllData();
+                }
+            });
+        });
     }
-
     init();
 
 })(jQuery)
